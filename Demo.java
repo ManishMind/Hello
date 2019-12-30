44e1dd14362bfcea7c94f02d1f6cf70c64d1c491
mkskrsri@gmail.com <mkskrsri@gmail.com>
2019-12-30 21:33:42 +0530

    this is 30th dec 201 9

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ public class Demo
 {
   public static void main(String args[])
    {
-       for(int i=1; i<=11;i++)
+       for(int i=1; i<=5;i++)
        {
           System.out.println("Hello Manish.... Ji PVR 30 dec 2019.. "+i);
        }
